47ab4f2e6520c57e18f5448ba9a4d94a1d32d36a
Neilgxz <43714202+Neilgxz@users.noreply.github.com>
2020-08-17 22:32:26 -0400

    kmp

--- 12_string_match/kmp.py
+++ 12_string_match/kmp.py
@@ -1,61 +1,87 @@
-"""
-    KMP algorithm
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 
-    Author: Wenru Dong
-"""
 
-from typing import List
+def kmp(main, pattern):
+    """
+    kmp字符串匹配
+    :param main:
+    :param pattern:
+    :return:
+    """
+    assert type(main) is str and type(pattern) is str
 
-def kmp(s: int, pattern: int) -> int:
-    m = len(pattern)
-    partial_match_table = _get_partial_match_table(pattern)
+    n, m = len(main), len(pattern)
+
+    if m == 0:
+        return 0
+    if n <= m:
+        return 0 if main == pattern else -1
+
+    # 求解next数组
+    next = get_next(pattern)
+    # 其实j-1记录了 已经有多少位连续匹配成功
     j = 0
-    for i in range(len(s)):
-        while j >= 0 and s[i] != pattern[j]:
-            j = partial_match_table[j]
-        j += 1
-        if j == m:
-            return i - m + 1
+    for i in range(n): 
+        # 在pattern[:j]中，从长到短递归去找最长的和后缀子串匹配的前缀字串，直到 j=0 或者 main[i] == pattern[j]
+        while j > 0 and main[i] != pattern[j]:
+            # 遇到坏字符，则模式串0~j-1为好前缀，计算next[j-1]得到最长的可以与后缀子串匹配的前缀字串的结尾下标
+            # 将j移动到这个最长匹配前缀的后一个位置，继续比较
+            j = next[j-1] + 1   
+
+        if main[i] == pattern[j]:
+            if j == m-1: # 对比到模式串的最后一位，全部匹配
+                return i-m+1
+            else: 
+                j += 1
+        # 一次循环结束，如果始终没有 main[i] == pattern[j]，i += 1 从下一位开始从模式串头开始比较。
+        # 如果j += 1 if结束后i也会+1 继续对比下一位 
     return -1
 
 
-def _get_partial_match_table(pattern: int) -> List[int]:
-    # Denote πᵏ(i) as π applied to i for k times,
-    # i.e., π²(i) = π(π(i)).
-    # Then we have the result:
-    #    π(i) = πᵏ(i-1) + 1,
-    # where k is the smallest integer such that
-    # pattern[πᵏ(i-1)+1] == pattern[i].
+def get_next(pattern):
+    """
+    next数组生成
 
-    # The value of π means the maximum length
-    # of proper prefix/suffix.
-    # The index of π means the length of the prefix
-    # considered for pattern.
-    # For example, π[2] means we are considering the first 2 characters
-    # of the pattern.
-    # If π[2] == 1, it means for the prefix of the pattern, P[0]P[1],
-    # it has a maximum length proper prefix of 1, which is also the
-    # suffix of P[0]P[1].
-    # We also add a π[0] == -1 for easier handling of boundary
-    # condition.
-    
+    注意：
+    理解的难点在于next[i]根据next[0], next[1]…… next[i-1]的求解
+    next[i]的值依赖于前面的next数组的值，求解思路：
+    1. 首先取出前一个最长的匹配的前缀子串，其下标就是next[i-1]
+    2. 对比下一个字符，如果匹配，直接赋值next[i]为next[i-1]+1，因为i-1的时候已经是最长
+    *3. 如果不匹配，需要递归去找次长的匹配的前缀子串，这里难理解的就是递归地方式，next[i-1]
+        是i-1的最长匹配前缀子串的下标结尾，则 *next[next[i-1]]* 是其次长匹配前缀子串的下标
+        结尾
+    *4. 递归的出口，就是在次长前缀子串的下一个字符和当前匹配 或 遇到-1，遇到-1则说明没找到任
+        何匹配的前缀子串，这时需要找pattern的第一个字符对比
+
+    ps: next[m-1]的数值其实没有任何意义，求解时可以不理。网上也有将next数组往右平移的做法。
+    :param pattern:
+    :return:
+    """
     m = len(pattern)
-    π = [0] * (m + 1)
-    π[0] = k = -1  # We use k here to represent πᵏ(i)
-    for i in range(1, m + 1):
-        while k >= 0 and pattern[k] != pattern[i - 1]:
-            k = π[k]
-        k += 1
-        π[i] = k
-    return π
+    next = [-1] * m
+
+    next[0] = -1 # 好前缀只有一个元素，没有前缀和后缀
+
+    # for i in range(1, m):
+    for i in range(1, m-1):
+        j = next[i-1]       # 取i-1时匹配到的最长前缀子串
+        while j != -1 and pattern[j+1] != pattern[i]:
+            j = next[j]     # 次长的前缀子串的结尾下标，即是next[next[i-1]]
+
+        # 根据上面跳出while的条件，当j=-1时，需要比较pattern[0]和当前字符
+        # 如果j!=-1，则pattern[j+1]和pattern[i]一定是相等的
+        if pattern[j+1] == pattern[i]:  # 如果接下来的字符也是匹配的，那i的最长前缀子串下标是next[i-1]+1
+            j += 1
+        next[i] = j
+
+    return next
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    m_str = "aabbbbaaabbababbabbbabaaabb"
+    p_str = "abbabbbabaa"
 
-    s = "abc abcdab abcdabcdabde"
-    pattern = "bcdabd"
-    print(kmp(s, pattern), s.find(pattern))
-
-    s = "hello"
-    pattern = "ll"
-    print(kmp(s, pattern), s.find(pattern))
+    print('--- search ---')
+    print('[Built-in Functions] result:', m_str.find(p_str))
+    print('[kmp] result:', kmp(m_str, p_str))
